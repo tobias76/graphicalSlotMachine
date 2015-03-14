967641da1b6e5b5cd64807965e4b04a9f607b6a1
Toby <tobyreed0@gmail.com>
2015-03-14 13:27:19 +0000

    Removed the new tab folder. No idea what it was.

--- net/Toby/GSM/Display/Display.py
+++ net/Toby/GSM/Display/Display.py
@@ -7,6 +7,7 @@ pygame.init()
 displayWidth = 800
 displayHeight = 600
 
+# Declares the screen size and makes it fullscreen.
 screen = pygame.display.set_mode((displayWidth, displayHeight), pygame.FULLSCREEN)
 
 pygame.display.set_caption("Graphical Slot Machine")
